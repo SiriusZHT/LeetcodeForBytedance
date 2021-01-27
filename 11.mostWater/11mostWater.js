5f80b216f169c6fb0c2ed5155c50a58009543c5b
SiriusZHT <49780895+SiriusZHT@users.noreply.github.com>
2021-01-27 17:45:33 +0800

    main

--- 11.mostWater/11mostWater.js
+++ 11.mostWater/11mostWater.js
@@ -2,13 +2,16 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function (height) {
-    let x, y = 0, i = 0, l = height.length - 1;
-    while (i < l) if ((x = (l - i) * (height[i] > height[l] ? height[l--] : height[i++])) > y) y = x;
+var maxArea = function(height) {
+    let x, y = 0,
+        i = 0,
+        l = height.length - 1;
+    while (i < l)
+        if ((x = (l - i) * (height[i] > height[l] ? height[l--] : height[i++])) > y) y = x;
     return y;
 };
-
-// 作者：xin-ru-zhi-shui-7t
-// 链接：https://leetcode-cn.com/problems/container-with-most-water/solution/sheng-zui-duo-shui-de-rong-qi-js3xing-by-xin-ru-zh/
-// 来源：力扣（LeetCode）
-// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+    // 作者：xin-ru-zhi-shui-7t
+    // 链接：https://leetcode-cn.com/problems/container-with-most-water/solution/sheng-zui-duo-shui-de-rong-qi-js3xing-by-xin-ru-zh/
+    // 来源：力扣（LeetCode）
+    // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
